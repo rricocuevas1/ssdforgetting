b2adecff9f5580b171bb3fe1135939fcfec0ed3e
rricocuevas1 <ramon.rico.cuevas@gmail.com>
2024-10-23 22:50:10 +0200

    Updated files

--- run_tmux_sessions.sh
+++ run_tmux_sessions.sh
@@ -14,7 +14,7 @@ session_number=1
 for config in "${configs[@]}"; do
     for run in {1..10}; do
         session_name="session_${session_number}"
-        full_command="$command $config; exec bash"
+        full_command="$command $config; bash"
         tmux new-session -d -s $session_name "$full_command"
         echo "Started Tmux session $session_name with command: $full_command"
         ((session_number++))
