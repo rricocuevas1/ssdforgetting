48bd3227c8af5442571e7d0ad5fa095876afe605
rricocuevas1 <ramon.rico.cuevas@gmail.com>
2024-11-08 20:39:30 +0100

    Updated files

--- run_tmux_sessions.sh
+++ run_tmux_sessions.sh
@@ -4,13 +4,20 @@
 command="python3 main.py"
 configs=(
     "flight 1 1 0.1 10000 0 0"
+    "flight 1 1 0.25 10000 0 0"
+    "flight 1 1 0.5 10000 0 0"
+    "photo 1 1 0.02 10000 0 0"
+    "photo 1 1 0.05 10000 0 0"
+    "photo 1 1 0.1 10000 0 0"
+    "wiki 1 1 0.1 10000 0 0"
     "wiki 1 1 0.25 10000 0 0"
+    "wiki 1 1 0.5 10000 0 0"
 )
 
-# Loop through each configuration and run the command 5 times for each
+# Loop through each configuration and run the command 10 times for each
 session_number=1
 for config in "${configs[@]}"; do
-    for run in {1..1}; do
+    for run in {1..10}; do
         session_name="session_${session_number}"
         full_command="$command $config; bash"
         tmux new-session -d -s $session_name "$full_command"
