6772b52c623add0d57c43655a720f767f3f1733b
rricocuevas1 <ramon.rico.cuevas@gmail.com>
2025-03-24 22:33:59 +0100

    testing

--- synthetic_data_generation.py
+++ synthetic_data_generation.py
@@ -32,14 +32,14 @@ def generate_synthetic_data(n_rows, n_queries):
     print(f"Generated: sample_data/synthetic/queries_{n_rows}_{n_queries}.csv")
 
 
-def convert_files_to_single_parquet(directory_path, csv_list):
+def convert_files_to_parquet(directory_path, csv_list):
     for filename in csv_list:
         csv_filepath = os.path.join(directory_path, filename)
         parquet_filename = os.path.splitext(filename)[0] + ".parquet"
         parquet_filepath = os.path.join(directory_path, parquet_filename)
 
         ddf = dd.read_csv(csv_filepath)
-        ddf.to_parquet(parquet_filepath, single_file=True)
+        ddf.to_parquet(parquet_filepath)
 
         print(f"Successfully converted '{csv_filepath}' to single Parquet file '{parquet_filepath}'")
 
