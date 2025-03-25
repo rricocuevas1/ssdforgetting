d7efd76cb0a0a400a5c8b66990ae056559ac0024
rricocuevas1 <ramon.rico.cuevas@gmail.com>
2025-03-25 14:23:06 +0100

    update

--- indep_df.py
+++ indep_df.py
@@ -16,21 +16,16 @@ def compute_weight(n_rows, queries, n_queries):
     return weight
 
 
-def compute_weight_parallel(n_rows, queries, n_queries):
-    """
-        Parallel weight computation
-    """
-    # Define helper functions
-    def chunked_iterable(iterable, chunk_size):
+def chunked_iterable(iterable, chunk_size):
         it = iter(iterable)
         while True:
             chunk = list(islice(it, chunk_size))
             if not chunk:
                 break
             yield chunk
-        
-    def process_queries_chunk(args):
-        n_rows, queries_chunk = args
+
+def process_queries_chunk(args):
+        n_rows, queries_chunk, n_queries = args
         chunk_size = len(queries_chunk)
         weight_chunk = np.zeros(n_rows)
         for j in range(chunk_size):
@@ -40,6 +35,10 @@ def compute_weight_parallel(n_rows, queries, n_queries):
             np.add.at(weight_chunk, answer_set, per_element_weight)
         return weight_chunk
 
+def compute_weight_parallel(n_rows, queries, n_queries):
+    """
+        Parallel weight computation
+    """
     weight = np.zeros(n_rows)
     n_cpus = cpu_count()
     chunk_size = (n_queries + n_cpus - 1) // n_cpus
@@ -47,7 +46,7 @@ def compute_weight_parallel(n_rows, queries, n_queries):
 
     # Execute the computation in parallel
     with Pool(n_cpus) as pool:
-        results = pool.map(process_queries_chunk, [(n_rows, chunk) for chunk in queries_g])
+        results = pool.map(process_queries_chunk, [(n_rows, chunk, n_queries) for chunk in queries_g])
     for weight_chunk in results:
         weight += weight_chunk
     return weight
